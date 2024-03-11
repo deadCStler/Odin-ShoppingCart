c47674591595608007657c96896b817fd5d40cba
deadCStler <chitranshsrivastava01@gmail.com>
2024-03-11 21:01:05 +0530

    added dummydata for brand dev page

--- src/components/brand/BrandDev.jsx
+++ src/components/brand/BrandDev.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import BrandCSS from "./Brand.module.css";
+import MiniBuyEvents from "./Events";
+
 function BrandDev() {
-  return <h1>Brand Dev Page</h1>;
+  const [year, setYear] = useState(2022);
+  return (
+    <>
+      <h1 className={BrandCSS.heading}>Development</h1>
+      <div className={BrandCSS.navGrid}>
+        <div onClick={() => setYear(2022)}>2022</div>
+        <div onClick={() => setYear(2021)}>2021</div>
+        <div onClick={() => setYear(2020)}>2020</div>
+        <div onClick={() => setYear(2019)}>2019</div>
+        <div onClick={() => setYear(2018)}>2018</div>
+      </div>
+      <div className={BrandCSS.content}>
+        <div>
+          <h1>{year}</h1>
+          <p>Year</p>
+        </div>
+        <div>
+          <MiniBuyEvents year={year} />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default BrandDev;
